74dc610993881d166d2ca570c27490d2e15e4884
Yelyzaveta Veis <liza.veis14@gmail.com>
2023-01-09 03:14:27 +0200

    production: + tests

---
+++ src/data/main-data.ts
@@ -0,0 +1,8 @@
+const mainData = {
+  search: 'Ice',
+  level: 'easy',
+  duration: '5_x_10',
+  levelNotSet: '',
+};
+
+export { mainData };
---
+++ wdio.conf.ts
@@ -0,0 +1,44 @@
+import type { Options } from '@wdio/types';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { APP_URL } = process.env;
+
+const config: Options.Testrunner = {
+  autoCompileOpts: {
+    autoCompile: true,
+    tsNodeOpts: {
+      transpileOnly: true,
+      project: './tsconfig.json',
+    },
+  },
+  specs: ['./src/specs/**/*.spec.ts'],
+  suites: {
+    base: ['./src/specs/base/**'],
+    advanced: ['./src/specs/advanced/**'],
+  },
+  maxInstances: 10,
+  capabilities: [
+    {
+      maxInstances: 5,
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+    },
+  ],
+  logLevel: 'silent',
+  bail: 0,
+  baseUrl: APP_URL,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ['chromedriver'],
+  framework: 'mocha',
+  reporters: [['spec', { showPreface: false }]],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000,
+  },
+};
+
+export { config };
